03bba350a3c4c4f3cc160372b00d08b5c814d731
Derek Baker <derek.baker89@gmail.com>
2021-10-02 12:25:50 +0100

    update terraform to use new index page

--- infrastructure/static-hosting.tf
+++ infrastructure/static-hosting.tf
@@ -2,15 +2,15 @@ resource "aws_s3_bucket" "static-hosting" {
   bucket = "${local.application-slug}-static-hosting"
   acl    = "public-read"
   website {
-    index_document = "main.html"
+    index_document = "index.html"
   }
 
 }
 resource "aws_s3_bucket_object" "object" {
   bucket       = aws_s3_bucket.static-hosting.bucket
-  key          = "main.html"
-  source       = "${path.cwd}/../main.html"
+  key          = "index.html"
+  source       = "${path.cwd}/../index.html"
   acl          = "public-read"
   content_type = "text/html"
-  etag         = filemd5("${path.cwd}/../main.html")
+  etag         = filemd5("${path.cwd}/../index.html")
 }
